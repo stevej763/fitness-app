253e9c78c813b87bcf0eb8eb34a872a4c6d21c25
Steve Jones <steve.jones763@gmail.com>
2020-08-16 13:41:51 +0100

    xcode project created

---
+++ fitness-app/fitness-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitness-app
+//
+//  Created by Steve Jones on 16/08/2020.
+//  Copyright © 2020 95Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
